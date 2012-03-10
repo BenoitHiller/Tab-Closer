12c721e71276ec604fcc65b5c658c6f45e87ec1c
Stephen R. Hamilton <stephen.r.hamilton@gmail.com>
2012-03-10 16:29:05 -0500

    adding some jasmine tests

---
+++ spec/ButtonSpec.js
@@ -0,0 +1,47 @@
+var chrome = function(){};
+chrome.tabs = function(){};
+chrome.windows = function(){};
+chrome.tabs.remove = function(array) {
+  return array;
+};
+chrome.browserAction = function(){};
+chrome.browserAction.onClicked = function(){};
+chrome.browserAction.onClicked.addListener = function(a){return a;};
+
+chrome.tabs.query = function(thing,func) {
+  if (!thing.windowId){
+    func([{"id":2}]);
+  }
+  else {
+    func([]);
+  }
+}
+
+chrome.windows.getCurrent = function(func) {
+  func({"id":1});
+}
+
+describe('Button', function () {
+  beforeEach(function () {
+    var localStorage = {};
+    init();
+  });
+
+  it('should close tabs',function() {
+    expect(tab_closer.close_tabs([{"id":2}])).toEqual(true);
+  });
+
+  it('should locate and close tabs', function () {
+    spyOn(tab_closer,"close_tabs");
+    tab_closer.check_window_close_tabs({"id":2});
+    expect(tab_closer.close_tabs).toHaveBeenCalledWith([{"id":2}]);
+  });
+
+  it('should respect the close by window setting',function() {
+    localStorage["tab-closer-window-setting"] = "true";
+    spyOn(tab_closer,"close_tabs");
+    tab_closer.check_window_close_tabs({"id":2});
+    expect(tab_closer.close_tabs).toHaveBeenCalledWith([]);
+  });
+
+});
